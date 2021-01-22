e8319630bf43fc00c25d02928c3bb255262bbbd6
ROMANO Cyril <romano.cyril@gmail.com>
2021-01-22 15:39:47 +0100

    First commit

---
+++ src/main/java/org/cro/springbanque/SpringBanqueApplication.java
@@ -0,0 +1,13 @@
+package org.cro.springbanque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBanqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBanqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/cro/springbanque/SpringBanqueApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cro.springbanque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBanqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
